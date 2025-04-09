0b9a6c905691d4c94cfcad299ad61ac8106bc3d4
kizzis <iswater.kim@gmail.com>
2025-04-09 15:38:42 +0900

    test: #3 프로모션 상세 조회 및 종료 api 기능 테스트

--- promotion/src/test/java/com/tweaty/promotion/application/PromotionServiceTest.java
+++ promotion/src/test/java/com/tweaty/promotion/application/PromotionServiceTest.java
@@ -5,19 +5,43 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tweaty.promotion.application.dto.PromotionCreateResponse;
+import com.tweaty.promotion.application.dto.PromotionReadResponse;
+import com.tweaty.promotion.domain.model.EventStatus;
+import com.tweaty.promotion.domain.model.Promotion;
+import com.tweaty.promotion.domain.repository.PromotionRepository;
 import com.tweaty.promotion.presentation.request.PromotionCreateRequest;
 
 @SpringBootTest
+@Transactional
 @ActiveProfiles("test")
 class PromotionServiceTest {
 	@Autowired
 	private PromotionService promotionService;
+	@Autowired
+	private PromotionRepository promotionRepository;
+	private Promotion testPromotion;
+
+	@BeforeEach
+	void setUp() {
+		testPromotion = Promotion.builder()
+			.eventName("5월 이벤트")
+			.eventDescription("5월 진행되는 이벤트입니다.")
+			.eventStatus(EventStatus.READY)
+			.couponId(UUID.randomUUID())
+			.eventStartAt(LocalDateTime.of(2025, 5, 1, 12, 0))
+			.eventEndAt(LocalDateTime.of(2025, 5, 8, 23, 59))
+			.build();
+
+		promotionRepository.save(testPromotion);
+	}
 
 	@Test
 	public void createPromotionTest() {
@@ -37,5 +61,28 @@ class PromotionServiceTest {
 		// then
 		assertThat(response.eventId()).isNotNull();
 		System.out.println("response.eventId() = " + response.eventId());
+	}
+
+	@Test
+	public void getPromotionTest() {
+		// given
+		// when
+		PromotionReadResponse response = promotionService.getEvent(testPromotion.getPromotionId());
+		// then
+		assertThat(response.eventId()).isNotNull();
+		assertThat(response.eventId()).isEqualTo(testPromotion.getPromotionId());
+		assertThat(response.eventName()).isEqualTo(testPromotion.getEventName());
+		assertThat(response.eventStatus()).isEqualTo(testPromotion.getEventStatus());
+	}
+
+	@Test
+	public void updateEventStatusToEndedTest() {
+		// given
+		// when
+		promotionService.updateEventStatusToEnded(testPromotion.getPromotionId());
+		// then
+		//Promotion updated = promotionRepository.findByPromotionId(testPromotion.getPromotionId()).get();
+
+		assertThat(testPromotion.getEventStatus()).isEqualTo(EventStatus.ENDED);
 	}
 }
