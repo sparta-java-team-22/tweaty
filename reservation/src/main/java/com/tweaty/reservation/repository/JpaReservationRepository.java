797d1be85e109cd6cfd8197fdcae3fc15f8ee7b3
josephuk77 <josephuk77@gmail.com>
2025-04-10 14:14:29 +0900

    feat : #6 repository 생성

---
+++ reservation/src/main/java/com/tweaty/reservation/repository/JpaReservationRepository.java
@@ -0,0 +1,12 @@
+package com.tweaty.reservation.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tweaty.reservation.domain.model.Reservation;
+import com.tweaty.reservation.domain.repository.ReservationRepository;
+
+public interface JpaReservationRepository extends JpaRepository<Reservation, UUID>, ReservationRepository {
+	
+}
