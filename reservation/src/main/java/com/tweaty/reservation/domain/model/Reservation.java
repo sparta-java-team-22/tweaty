a4a7a599fe051a2ff059c1135d956416a5ed8b5a
josephuk77 <josephuk77@gmail.com>
2025-04-10 14:10:34 +0900

    feat : #6 entity, requestDto 생성

---
+++ reservation/src/main/java/com/tweaty/reservation/domain/model/Reservation.java
@@ -0,0 +1,61 @@
+package com.tweaty.reservation.domain.model;
+
+import java.util.UUID;
+
+import com.tweaty.reservation.presentation.request.ReservationRequestDto;
+
+import base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "p_reservation")
+public class Reservation extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
+
+	@Column(name = "store_id", nullable = false)
+	private UUID storeId;
+
+	@Column(name = "table_count", nullable = false)
+	private int tableCount;
+
+	@Column(name = "reservation_table_count", nullable = false)
+	private int reservationTableCount;
+
+	@Column(name = "reservation_time", nullable = false)
+	private String reservationTime;
+
+	@Column(name = "reservation_date", nullable = false)
+	private String reservationDate;
+
+	@Column(name = "current_capacity", nullable = false)
+	private int currentCapacity;
+
+	@Column(name = "table_two", nullable = false)
+	private int tableTwo;
+
+	@Column(name = "table_four", nullable = false)
+	private int tableFour;
+
+	Reservation(ReservationRequestDto reservationRequestDto) {
+		this.storeId = reservationRequestDto.getStoreId();
+		this.tableCount = reservationRequestDto.getTableCount();
+		this.reservationTableCount = reservationRequestDto.getReservationTableCount();
+		this.reservationTime = reservationRequestDto.getReservationTime();
+		this.reservationDate = reservationRequestDto.getReservationDate();
+		this.currentCapacity = reservationRequestDto.getCurrentCapacity();
+		this.tableTwo = reservationRequestDto.getTableTwo();
+		this.tableFour = reservationRequestDto.getTableFour();
+	}
+}
