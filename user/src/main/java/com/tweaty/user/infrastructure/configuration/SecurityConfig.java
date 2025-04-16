a8715266adec8ed776c5cc076418382773b8b642
srrrn <plmnji808@gmail.com>
2025-04-16 23:06:44 +0900

    fix: @PreAuthorize 문제 해결

--- user/src/main/java/com/tweaty/user/infrastructure/configuration/SecurityConfig.java
+++ user/src/main/java/com/tweaty/user/infrastructure/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.tweaty.user.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +13,7 @@ import com.tweaty.user.infrastructure.filter.PreAuthHeaderFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	private final PreAuthHeaderFilter preAuthHeaderFilter;
